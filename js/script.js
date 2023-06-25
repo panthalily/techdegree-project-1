a2d19b31569febd0885a4bdeb6879840106d0af2
Amy Griffiths <76974409+panthalily@users.noreply.github.com>
2023-06-25 17:10:40 +0100

    Updated comments & added RBG/intervals

--- js/script.js
+++ js/script.js
@@ -7,75 +7,94 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
+/* 
+ `quotes` array 
+  This creates an array literal and stores object literals within each array item, i.e. quote.
+
+*/
 
 const quotes = [
-  { quote: '"Not all those who wander are lost."',
-    name: "J.R.R Tolkein,",
-    citation: "The Fellowship of the Ring, Book.",
-    year: 1954.,
-    tags: "Fantasy."
+  { quote: "Not all those who wander are lost",
+    source: "Gandalf",
+    citation: "J.R.R Tolkein, The Fellowship of the Ring, Book 1 Chapter 10",
+    year: "1954.",
   },
-  { quote: "Even the smallest person can change the course of the future.",
-    name: "Galadriel.",
-    citation: "The Fellowship Of The Ring, Movie.",
-     year: 2001.
-   } ,
-  { quote: "It's a Dangerous Business, Frodo, Going Out Your Door.",
-    name: "Frodo Baggins, Quoting Bilbo Baggins.",
-    citation: "The Fellowship Of The Ring, Book.",
-     year:  2001., 
-     tags: "Fantasy."
+  { quote: "Even the smallest person can change the course of the future",
+    source: "Galadriel",
+    citation: "The Fellowship Of The Ring, Film",
+    year: "2001.",
+    tags: "Fantasy, Adventure."
+   },
+  { quote: "It's a dangerous business, Frodo, going out your door. You step onto the road, and if you don't keep your feet, there's no knowing where you might be swept off to.",
+    source: "Frodo Baggins, Quoting Bilbo Baggins",
+    citation: "J.R.R Tolkein, The Fellowship Of The Ring, Book 1 Chapter 3",
+    year: "1954.", 
   },
-  { quote: "'There's Some Good in This World, Mr. Frodo, and It's Worth Fighting For.'",
-    name: "Samwise Gamgee.",
-    citation: "The Two Towers, Movie." ,
-     year:  2001.
+  { quote: "There's Some Good in This World, Mr. Frodo, and It's Worth Fighting For.",
+    source: "Samwise Gamgee",
+    citation: "The Two Towers, Movie",
+    year:  "2001.",
+    tags: "Classics, Adventure."
   } ,
-  { quote: '"What About Second Breakfast?"',
-    name : "Peregrin Took.",
-    tags: "Fantasy."
+  { quote: "We've had one, yes. What About Second Breakfast?",
+    source: "Peregrin Took",
+    citation: "J.R.R Tolkein, The Fellowship of the Ring, Movie",
   }
 ];
 
 
-/***
- * `getRandomQuote` function
-***/
+/*
+  `getRandomQuote` function
+   creates a random array Index number by multiplying a random number greater than 0 but less than one, then multiplying it by the quotes.length (0-4). 
+   Then rounding it down to a whole interger. This returns the random quotes array index number.
+*/
 
 function getRandomQuote() {
   let arrayIndex = Math.floor(Math.random() * quotes.length);
   return quotes[arrayIndex]
 };
+/*
+  `randomRGB` function
+    creates a random r/g/b number by multiplying a random number greater than 0 but less than one, then multiplying it by 256. Then rounding it down to a whole interger.
+    returns a random hex number and changes background colour by using the background property in the HTML file.
+*/
+
+function randomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  document.body.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
 
 
-/***
- * `printQuote` function
-***/
+/*
+  `printQuote` function
+  returns the value stored in the quotes array against a html tag by using the getRandomQuote function created above. Using template literals, it displays the value
+  against the quote object, i.e. source & quote.
+  This message is displayed using the displayRandomQuote variable into the 'quote-box' and a random background colour is created, thanks to the randomRGB function created above.
+  The printQuote also tests whether there is year, tag & citation obejcts within the array, if there are printQuote also displays these, if not it displays nothing for these tags.
+*/
 
 function printQuote() {
-  let randomQuotes = getRandomQuote();
-  let  displayRandomQuote = `<p class='quote'> ${randomQuotes.quote}</p>
-                             <p class='source'> ${randomQuotes.source}`
-        if (quotes.citation) {
-            displayRandomQuote += `<span class='citation'> ${randomQuotes.citation} </span>`;
-        } else {
-          displayRandomQuote += "";
+    let randomQuote = getRandomQuote();
+    let  displayRandomQuote = `<p class='quote'>${randomQuote.quote}</p>
+                             <p class='source'><strong>${randomQuote.source}</strong>`
+        if (randomQuote.citation) {
+            displayRandomQuote += `<span class="citation">${randomQuote.citation}</span>`;
         }
-        if (quotes.year) {
-            displayRandomQuote += `<span class='year'> ${randomQuotes.year} </span>`;
-        } else {
-          displayRandomQuote += "";
-        } if (quotes.tags) {
-          displayRandomQuote += `<p class='tags'> ${randomQuotes.tags} </p>`;
-        } else {
-          displayRandomQuote += ""; 
+        if (randomQuote.year) {
+            displayRandomQuote += `<span class="year">${randomQuote.year}</span>`;
+        } if (randomQuote.tags) {
+          displayRandomQuote += `<span class='tags'><i> ${randomQuote.tags}<i></span></p>`;
         }
+        document.getElementById('quote-box').innerHTML = displayRandomQuote;
+        randomRGB();
   }
 
-console.log(printQuote());
+ 
+// prints a random quote every 5500 milliseconds.
+setInterval (printQuote, 5500);
+
 
 /***
  * click event listener for the print quote button
